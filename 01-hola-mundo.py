54ed60fa95cfa6f5d807e3806f3b2118dff7b9bf
Eduardo <ederiveroman@gmail.com>
2025-03-20 20:58:49 -0500

    introduccion a python

---
+++ 01-hola-mundo.py
@@ -0,0 +1,23 @@
+nombre = 'Eduardo'
+print(type(nombre))
+nombre = 30
+print(type(nombre))
+nombre = False
+print(type(nombre))
+nombre = 10.5
+
+# Necesario para la programacion porque sirve como ayuda a otros programadores a entender el funcionamiento
+
+# Para imprimir por consola en Python
+print('Hola mundo')
+print(nombre)
+
+# Para saber el tipo de variable
+print(type(nombre))
+
+texto = "Buenas noches con todos Brian O'Conner"
+texto = '"Buenas noches con todos" Dijo Abigail'
+
+otro_texto = "Y eduardo dijo: \"Hola amigos!\""
+
+print(otro_texto)
