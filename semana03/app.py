dad5926cb0c711a769d41cc60a5c19828109f5cf
Eduardo <ederiveroman@gmail.com>
2025-04-02 21:24:37 -0500

    uso de migraciones

--- semana03/app.py
+++ semana03/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+# Esta clase nos va a ayudar a poder gestionar las migraciones de nuestro proyecto con la bd
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/prueba_flask'
+# al usar flask-sqlalchemy necesita de la instancia de flask la conexion a la base de datos
+db = SQLAlchemy(app=app)
 
+# ya con la instanciacion de la clase ya podemos comenzar a utilizar nuestras migraciones
+Migrate(app, db)
 
 @app.route('/')
 def inicio():
