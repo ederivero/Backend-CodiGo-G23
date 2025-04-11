d93d1fd7771795d7ed040d085461a4e338967fc9
Eduardo <ederiveroman@gmail.com>
2025-04-10 23:01:13 -0500

    uso de serializadores

--- semana04/usuarios/usuarios_controller.py
+++ semana04/usuarios/usuarios_controller.py
@@ -5,6 +5,8 @@ from modelos import Usuario
 from flask import request
 from .usuarios_serializer import UsuarioSerializer
 from marshmallow.exceptions import ValidationError
+from bcrypt import gensalt, hashpw
+from sqlalchemy.exc import IntegrityError
 
 # Es para mantener nuestro proyecto de flask en forma de modulos, es decir todo lo relacionado con los usuarios estaran en este modulo y trabajara de manera aislada con los demas modulo
 usuarios_blueprint = Blueprint('usuarios_bp', __name__)
@@ -15,11 +17,26 @@ class Registro(Resource):
         data = request.get_json()
         try:
             dataSerializada = UsuarioSerializer().load(data)
+            # Aca intervenimos para generar el hash de la password
+            # salt es un texto aleatorio que luego se combinara con la contraseña para generar el hash
+            salt = gensalt()
+            # para el metodo hashpw tenemos que convertir la password a bytes y mediante su encodificacion definimos que los caracteres estaran en utf-8
+            password = bytes(dataSerializada.get('password'),'utf-8')
+            hashedPassword = hashpw(password,salt)
+            print(hashedPassword)
+            # convertimos el hashed password de bytes a string para poderlo guardar en la bd
+            nuevaPassword = hashedPassword.decode('utf-8')
+            dataSerializada['password']=nuevaPassword
+
             nuevoUsuario = Usuario(**dataSerializada)
-            print(nuevoUsuario)
+            bd.session.add(nuevoUsuario)
+
+            bd.session.commit()
+            resultado = UsuarioSerializer().dump(nuevoUsuario)
 
             return {
-                'message': 'Usuario creado exitosamente'
+                'message': 'Usuario creado exitosamente',
+                'content': resultado
             },201 # Created
         # Si al momento de hacer la validacion falla entonces para identificar que el error es por la validacion en el except agregamos esa condicion
         except ValidationError as error:
@@ -27,5 +44,14 @@ class Registro(Resource):
                 'message':'Error al crear el usuario',
                 'content':error.args # los args es la propiedad donde se guarda el mensaje del error
             },400 # Bad Request
+        except IntegrityError as error:
+            return {
+                'message': 'Error al crear el usuario',
+                'content': 'Usuario con el correo ya existe'
+            }, 400
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        
 api.add_resource(Registro, '/registro')
--- semana04/usuarios/usuarios_serializer.py
+++ semana04/usuarios/usuarios_serializer.py
@@ -1,7 +1,23 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from marshmallow.validate import Email, Regexp
+from marshmallow import Schema, fields
 from modelos import Usuario
 
 class UsuarioSerializer(SQLAlchemyAutoSchema):
+    # si quiero modificar el comportamiento de uno de los atributos del modelo
+    # si queremos que este atributo sea solamente para cuando se quiera cargar (load)
+    # https://marshmallow-sqlalchemy.readthedocs.io/en/latest/api_reference.html#marshmallow_sqlalchemy.auto_field
+    # https://marshmallow.readthedocs.io/en/3.x-line/marshmallow.fields.html#base-field-class
+    password = auto_field(load_only=True, validate=[Regexp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@!#$%&*])[A-Za-z0-9@!#$%&*]{8,}$',error='La password debe de tener al menos una mayuscula, al menos una minuscula, al menos un digito y al menos un caracter especial, asi mismo no debe ser menor a 8 caracteres')])
+    # auto_field sirve para sobrescribir el comportamiento de un atributo del modelo
+    nombre = auto_field()
+    # Adicional a solamente mostrar o leer podemos tambien agregar validacion adicional como en el caso del correo para que se cumpla el formato establecido
+    correo = auto_field(validate=[Email(error='El correo no cumple con el formato correcto')])
+
     class Meta:
         model = Usuario
-    
+
+# Asi se crea un serializador sin la necesidad de tener un modelo
+class LoginSerializer(Schema):
+    correo = fields.Email(required=True)
+    password = fields.String(required=True)
