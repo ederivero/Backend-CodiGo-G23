734a3f1a60df52e170cc1f300f5b5b3368f02d31
Eduardo <ederiveroman@gmail.com>
2025-04-10 20:14:07 -0500

    inicio semana 04

---
+++ semana04/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from os import environ
+from dotenv import load_dotenv
+from instancias import bd
+from flask_migrate import Migrate
+
+# Leera las variables declaradas en el archivo .env y las pondra como variables de entorno
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
+# inicializo mi conexion a la base de datos
+bd.init_app(app)
+
+# inicializo mi sistema de migraciones en flask
+Migrate(app,bd)
+
+@app.route('/')
+def inicio():
+    return {
+        'message':'La aplicacion esta funcionado exitosamente'
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
